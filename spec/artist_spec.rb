3fadbec5f9726b8f79de582b764a94a32b215327
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-11 03:47:48 +0000

    Automatically backed up by Learn

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,5 +1,5 @@
-require_relative './spec_helper'
-require_relative '../config/environment'
+require_relative './spec_helper.rb'
+require_relative '../config/environment.rb'
 
 describe Artist do
   before(:each) do
