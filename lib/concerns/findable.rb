0b42a1f53e85c201f9c20a8898c61a3a15745ad5
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-11 03:37:59 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,10 @@
 # require_relative '../config/environment'
 
 modue Findable
-  def find_by_name(name)
-    $.detect{|a| a.name == name}
-    # self.detect{|a| a.name == name}
-   end
+  module ClassMethods
+    def find_by_name(name)
+      $.detect{|a| a.name == name}
+      # self.detect{|a| a.name == name}
+    end
+  end 
 end 
