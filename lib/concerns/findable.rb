d349a15d6e2d82e69d224dc0b669ae90be38ad16
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-11 02:55:36 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 modue Findable
   def find_by_name(name)
-    all.detect{|a| a.name == name}
-    all.detect{|a| a.name == name}
+    name.detect{|a| a.name == name}
+    name.detect{|a| a.name == name}
   end
 end 
