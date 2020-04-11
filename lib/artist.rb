f1bcad5c9d137c89fcce5f4ed91ba36e6ce769dc
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-11 03:41:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,11 +4,10 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
-  include Memorable
-  extend Memorable
-  include Paramable
-  include Findable
-  extend Findable
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
   
   @@artists = []
 
