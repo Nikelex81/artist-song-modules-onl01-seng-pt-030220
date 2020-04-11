eaaf6c40674493fbda2d9aae09dd0d29753ccc21
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-11 03:43:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,25 +9,25 @@ class Song
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   
-  @@songs = []
+  # @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
+  # def initialize
+  #   @@songs << self
   # end
 
-  def self.all
-    @@songs
-  end
+  # # def self.find_by_name(name)
+  # #   @@songs.detect{|a| a.name == name}
+  # # end
 
-  def artist=(artist)
-    @artist = artist
-  end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
+  # def self.all
+  #   @@songs
   # end
+
+  # def artist=(artist)
+  #   @artist = artist
+  # end
+
+  # # def to_param
+  # #   name.downcase.gsub(' ', '-')
+  # # end
 end
